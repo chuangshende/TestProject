b7ef5ca28ed6e62a2de7ef4672054c9c0d1fcd7a
Zengqc <2211730693@qq.com>
2020-03-21 15:51:11 +0800

    33333333

---
+++ app/src/main/java/qjtg/appzyh/myproject/TestJava.java
@@ -0,0 +1,12 @@
+package qjtg.appzyh.myproject;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+public class TestJava {
+    public static void show(Context context){
+        Log.e("My Project Test","My Project Test");
+        Toast.makeText(context,"My Project Test",Toast.LENGTH_LONG).show();
+    }
+}
